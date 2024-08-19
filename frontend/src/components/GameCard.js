0eb3b2be1c10fb8c8bfc4525d18ec6f003596a79
AlexBorchers22 <alex.borchers@piersonwireless.com>
2024-08-19 09:45:26 -0500

    Cleanup of unused code.

--- frontend/src/components/GameCard.js
+++ frontend/src/components/GameCard.js
@@ -2,15 +2,9 @@ import React from "react";
 import { red, blue } from "@mui/material/colors";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const GameCard = ({ game }) => {
   // Get first letter of word as a capital letter
