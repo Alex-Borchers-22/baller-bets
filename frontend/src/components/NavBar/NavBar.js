9e6eeddd10c9b92c425fc380dda02f2b1ec73388
AlexBorchers22 <alex.borchers@piersonwireless.com>
2024-08-20 19:17:10 -0500

    Navbar bug fix.

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -5,25 +5,11 @@ import PublicNavBar from "./PublicNavBar";
 // NavBar component
 const NavBar = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("bb_user")));
-
   useEffect(() => {
-    const handleStorageChange = () => {
-      setUser(JSON.parse(localStorage.getItem("bb_user")));
-    };
-
-    // Listen to the storage event
-    window.addEventListener("storage", handleStorageChange);
-
-    // Optional: Listen for changes within the same window
-    const handleUserChange = () => {
-      setUser(JSON.parse(localStorage.getItem("bb_user")));
-    };
-    window.addEventListener("bb_user_update", handleUserChange);
-
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-      window.removeEventListener("bb_user_update", handleUserChange);
-    };
+    const localUser = localStorage.getItem("bb_user");
+    if (localUser) {
+      setUser(JSON.parse(localUser));
+    }
   }, []);
 
   // If user does not exist, show public navbar
