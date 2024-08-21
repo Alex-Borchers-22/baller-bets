113975d4440db7279ce9b02ff1ab5a4f4068f2f8
AlexBorchers22 <alex.borchers@piersonwireless.com>
2024-08-20 19:23:14 -0500

    Modification to setting user credentials.

--- frontend/src/utils/setUserCredentials.js
+++ frontend/src/utils/setUserCredentials.js
@@ -1,7 +1,8 @@
 // Set user credentials in local storage
 const setUserCredentials = (user, accessToken) => {
-  console.log("Setting user credentials", user);
-  localStorage.setItem("bb_user", user);
+  // If user is an object, convert to string
+  const userString = typeof user === "object" ? JSON.stringify(user) : user;
+  localStorage.setItem("bb_user", userString);
   localStorage.setItem("bb_token", btoa(accessToken));
 };
 
