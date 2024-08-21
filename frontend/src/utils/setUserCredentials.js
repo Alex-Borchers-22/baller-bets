6fce16b7a10f7a0512ef685aeb8c2eca6a9e55ca
AlexBorchers22 <alex.borchers@piersonwireless.com>
2024-08-20 19:20:18 -0500

    Navbar bug fix.

--- frontend/src/utils/setUserCredentials.js
+++ frontend/src/utils/setUserCredentials.js
@@ -1,5 +1,6 @@
 // Set user credentials in local storage
 const setUserCredentials = (user, accessToken) => {
+  console.log("Setting user credentials", user);
   localStorage.setItem("bb_user", user);
   localStorage.setItem("bb_token", btoa(accessToken));
 };
